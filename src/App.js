f317fc19b911433ec5006433b633ce7d91c75454
Vikash Nayan <imvikashnayan@gmail.com>
2024-08-15 23:53:20 +0530

    fix image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Container, AppBar, Typography, Grid, Grow } from "@material-ui/core";
+import memories from "./images/memories.png";
 
 const App = () => {
   return (
-    <Container>
-      <AppBar>
-        <Typography></Typography>
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Hello Vikash Nayan
+        </Typography>
+        <img src={memories} alt="memories" height="160" />
       </AppBar>
     </Container>
   );
